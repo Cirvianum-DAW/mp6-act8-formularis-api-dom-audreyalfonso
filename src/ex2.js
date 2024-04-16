9c473805e5972b23f3216f72b46a00d982715692
audreyalfonso <audrey.alfonso@cirvianum.cat>
2024-04-16 23:27:58 +0200

    Ex2 - Fet

--- src/ex2.js
+++ src/ex2.js
@@ -1,6 +1,6 @@
 document
     .getElementById('peticioForm')
-    .addEventListener('submit', function (event) {
+    .addEventListener('submit', async function (event) {
         event.preventDefault();
 
         const city = document.getElementById('city').value;
@@ -8,26 +8,26 @@ document
         const country = document.getElementById('country').value;
 
         // Tots els camps del formulari són obligatoris
-        if (!city || !codiPostal || !country) {
-            alert('Cap camp pot estar buit');
+        if (!city || !country) {
+            alert('Els camps no poden estar buits');
             return;
         }
 
         try {
             // L'API requereix una key
             const apikey = 'aa114ce7fd4543f2999150710241004';
-            const currentUrl = `http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&lang=es`;
-            const forecastUrl = `http://api.weatherapi.com/v1/forecast.json?key=${apikey}&q=${city}&lang=es&days=2`;
+            const currentUrl = `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&lang=es`;
+            const forecastUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apikey}&q=${city}&lang=es&days=2`;
 
             const [currentResponse, forecastResponse] = await Promise.all([
                 fetch(currentUrl),
                 fetch(forecastUrl),
             ]);
 
-            cont [currentData, forecastData] = await Promise.all([
+            const [currentData, forecastData] = await Promise.all([
                 currentResponse.json(),
                 forecastResponse.json(),
-            ])
+            ]);
 
             console.log(currentData);
             console.log(forecastData);
@@ -35,26 +35,43 @@ document
             // Abans de fer els canvis a través del DOM, ens guardem la 
             // informació necessària que hem recuperat de l'API
             const weatherData = {
-                current:{
+                current: {
                     city: currentData.location.name,
                     country: currentData.location.country,
                     region: currentData.location.region,
                     weatherIcon: currentData.current.condition.icon,
                     temperature: `${currentData.current.temp_c}ºC`,
-                    weatherDescription: currentData.condition.text,
+                    weatherDescription: currentData.current.condition.text,
                 },
-                forecast:{
+                forecast: {
                     city: forecastData.location.name,
                     country: forecastData.location.country,
                     region: forecastData.location.region,
                     weatherIcon: forecastData.forecast.forecastday[1].day.condition.icon,
                     temperature: `${forecastData.forecast.forecastday[1].day.avgtemp_c}ºC`,
-                    weatherDescription: forecastData.forecast.forecastday[1].day.text,
+                    weatherDescription: forecastData.forecast.forecastday[1].day.condition.text,
                 }
             }
+
+            // Mostra els resultats a la pàgina
+            displayWeatherResults(weatherData);
 
         } catch (error) {
             console.error(error);
         }
-        
-    })
+    });
+
+function displayWeatherResults(weatherData) {
+    // Mostrar la informació actual
+    document.getElementById('location').textContent = `${weatherData.current.city}, ${weatherData.current.country}, ${weatherData.current.region}`;
+    document.getElementById('temperature').textContent = `Temperatura actual: ${weatherData.current.temperature}`;
+    document.getElementById('weatherDescription').textContent = `Descripció del temps actual: ${weatherData.current.weatherDescription}`;
+    document.getElementById('weatherIcon').src = weatherData.current.weatherIcon;
+    // Mostrar la informació de la previsió per el dia següent
+    document.getElementById('forecastTemperature').textContent = `Temperatura prevista per al dia següent: ${weatherData.forecast.temperature}`;
+    document.getElementById('forecastWeatherDescription').textContent = `Descripció del temps previst per al dia següent: ${weatherData.forecast.weatherDescription}`;
+    document.getElementById('forecastWeatherIcon').src = weatherData.forecast.weatherIcon;
+    // Mostrar els resultats
+    document.getElementById('weatherResults').classList.remove('hidden');
+}
+
